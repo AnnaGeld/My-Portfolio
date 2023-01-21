d492717851072215acbd56506e632d58667dd2c2
AnnaGeld <annageld508@gmail.com>
2023-01-21 15:48:47 +0400

    Added My portfolio to the github
    My first portfolio;)

---
+++ script.js
@@ -0,0 +1,13 @@
+const nav = document.querySelector('nav');
+const button = document.querySelector('.hamburger-menu')
+button.addEventListener('click', (event) => {
+    nav.classList.toggle('open')
+})
+
+
+
+window.addEventListener("scroll",function(){
+    const header = document.getElementById("header");
+    header.classList.toggle("sticky", window.scrollY > 0)
+})
+
